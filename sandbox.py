3c56fa2afd5431f415e166032ab2819896658a91
Shiba Prasad Aind <shiba@Shibas-MacBook-Pro.local>
2020-08-31 20:57:32 +0530

    Removed some code

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,4 @@
-import sys
 import requests
-
-# print(sys.print)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://google.com")
